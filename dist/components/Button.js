b5f8be6e551b7523b3d52b43e9014f904f045579
gowtham chilukuri <g.chilukuri@sigeosrl.com>
2024-09-23 14:50:41 +0200

    up

---
+++ dist/components/Button.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+var Button = function (_a) {
+    var label = _a.label, onClick = _a.onClick;
+    return _jsx("button", { onClick: onClick, children: label });
+};
+export default Button;
---
+++ dist/index.js
@@ -0,0 +1,3 @@
+// src/index.ts
+export { default as Button } from "./components/Button";
+// Add other component exports as needed
